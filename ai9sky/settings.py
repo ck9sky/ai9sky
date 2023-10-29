9f2fef8b3883b0334a04ca0bf610b50ec0588527
Chris Komaroff <ckom9@example.com>
2023-10-28 20:44:36 -0700

    ... configure new django project ...

--- ai9sky/settings.py
+++ ai9sky/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'ai9sky.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+"""
+Django Debug Toolbar - update INTERNAL_IPS to allow display. This was not necessary with DDTB 1.7 (before 1.8).
+https://django-debug-toolbar.readthedocs.io/en/stable/installation.html
+"""
+INTERNAL_IPS = ['127.0.0.1', ]
