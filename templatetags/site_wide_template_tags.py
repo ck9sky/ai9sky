c32561b624d86ed220a432ac49f580d7c904eadc
Chris Komaroff <ckom9@example.com>
2023-11-02 20:14:20 -0700

    ... 1st app ... chatgpt_api_js ...

--- templatetags/site_wide_template_tags.py
+++ templatetags/site_wide_template_tags.py
@@ -60,8 +60,8 @@ def cacheBusterFileNames():
     CB = {
         "ai9sky_js": f"js/ai9sky.js{urlQueryString}",
         "ai9sky_css": f"css/ai9sky.css{urlQueryString}",
-        #"chatgpt_api_js_css": f"chatgpt_api_js/css/chatgpt_api_js.css{urlQueryString}",
-        #"chatgpt_api_js_js": f"chatgpt_api_js/js/chatgpt_api_js.js{urlQueryString}",
+        "chatgpt_api_js_css": f"chatgpt_api_js/css/chatgpt_api_js.css{urlQueryString}",
+        "chatgpt_api_js_js": f"chatgpt_api_js/js/chatgpt_api_js.js{urlQueryString}",
         #"chatgpt_api_py_css": f"chatgpt_api_py/css/chatgpt_api_py.css{urlQueryString}",
         #"chatgpt_api_py_js": f"chatgpt_api_py/js/chatgpt_api_py.js{urlQueryString}",
         #"dalle_api_js_css": f"dalle_api_js/css/dalle_api_js.css{urlQueryString}",
