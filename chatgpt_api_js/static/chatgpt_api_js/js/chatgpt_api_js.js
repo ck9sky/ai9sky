4d8d84267a92ae0abc5eb2cdfec7662afc3c9197
Chris Komaroff <ckom9@example.com>
2023-11-06 01:51:35 -0800

    ... work on chatgpt_api_js app ...

--- chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
+++ chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
@@ -19,46 +19,35 @@ form.addEventListener('submit', e => {
     console.log(`value: ${value}`);  // ############################# 11/5/23
 
     $("#id_prompt").on("click", function(e){
-    // $("#id_prompt").on("submit", function(e){
 
-    // https://stackoverflow.com/questions/5651933/what-is-the-opposite-of-evt-preventdefault
-    // form.unbind('submit');  // ########### ???
-    // $(this).submit();
+        $.ajax({
+            // type: "POST",  ######### old?
+            /* I don't believe I have ever used type "GET" for the jQuery ajax(). BUT I need to avoid error 403 problems,
+               and I only need to "get" the OpenAI ChatGPT API key and assign to a LOCAL(!) javascipt variable. So using
+               type "POST" is (luckily) unnecessary. 11/5/23 */
+            type: "GET",  // ############# test?
+            url: '/chatgpt_api_js/test1/',
+            dataType: "json",
+            data: {},
+        })  // ajax()
+        .done(function(data){
+            if (typeof (data['chatgpt_api_key']) !== "undefined"){
+                /* HERE! Assign api key to a LOCAL javascript variable. This is the best security I can provide, but it
+                   still may not be enough? Picking off GLOBAL JS VARS is EASY in a browser inspector, but I suspect there
+                   are sophisticated tools that can read LOCAL vs vars too? Hopefully not, BUT THIS IS ANOTHER GOOD REASON
+                   TO USE PYTHON openai MODULE/PIP INSTEAD OF A "PURE" JAVASCRIPT SOLUTION. 11/5/23 */
 
-    // // Prevent hidden-form from submitting anything (stop page refresh).
-    // e.preventDefault();   // ############ ??
-    // e.stopPropagation();  // ###########??
+                let api = data['chatgpt_api_key'];
+                console.log(`api: ${api}\nvalue: ${value}`);  // ########### test
 
-    $.ajax({
-        // type: "POST",  ######### old?
-        /* I don't believe I have ever used type "GET" for the jQuery ajax(). BUT I need to avoid error 403 problems,
-           and I only need to "get" the OpenAI ChatGPT API key and assign to a LOCAL(!) javascipt variable. So using
-           type "POST" is (luckily) unnecessary. 11/5/23 */
-        type: "GET",  // ############# test?
-        url: '/chatgpt_api_js/test1/',
-        dataType: "json",
-        data: {},
-    })  // ajax()
-    .done(function(data){
-        if (typeof (data['chatgpt_api_key']) !== "undefined"){
-            /* HERE! Assign api key to a LOCAL javascript variable. This is the best security I can provide, but it
-               still may not be enough? Picking off GLOBAL JS VARS is EASY in a browser inspector, but I suspect there
-               are sophisticated tools that can read LOCAL vs vars too? Hopefully not, BUT THIS IS ANOTHER GOOD REASON
-               TO USE PYTHON openai MODULE/PIP INSTEAD OF A "PURE" JAVASCRIPT SOLUTION. 11/5/23 */
-
-            let api = data['chatgpt_api_key'];
-            console.log(`api: ${api}\nvalue: ${value}`);  // ########### test
-
-            if (value !== '') {
-
-
-                createMessageInstance();
-                askChatGPT(api);
-                handleScroll();
-                promptInput.value = '';
+                if (value !== '') {
+                    createMessageInstance();
+                    askChatGPT(api);
+                    handleScroll();
+                    promptInput.value = '';
+                }
             }
-        }
-    });  // .done()
+        });  // .done()
     });
     $("#id_prompt").click();
 
