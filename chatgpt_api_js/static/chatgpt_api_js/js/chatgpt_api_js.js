0d5a2830a596bb55bb2b220a65c950275f860013
Chris Komaroff <ckom9@example.com>
2023-11-05 20:57:17 -0800

    ... work on chatgpt_api_js app ...

--- chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
+++ chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
@@ -2,15 +2,17 @@ api = undefined;
 
 
 $("#prompt").on("click", function(event){
-    $.get("/chatgpt_api_js/test1redirect/", function (data){
-        if (JSON.stringify(data)!=='{}'){
-            if (typeof(data['chatgpt_api_key'])!=="undefined" && data['chatgpt_api_key']!==''){
-                // DANGER: api must be a local variable to prevent security hole!
-                api = data['chatgpt_api_key']; // From Django
+    if (typeof(api)==="undefined") {
+        $.get("/chatgpt_api_js/test1redirect/", function (data) {
+            if (JSON.stringify(data) !== '{}') {
+                if (typeof (data['chatgpt_api_key']) !== "undefined" && data['chatgpt_api_key'] !== '') {
+                    // DANGER: api must be a local variable to prevent security hole!
+                    api = data['chatgpt_api_key']; // From Django
 
-                alert(`api = ${api}`);   // ################## test, remove !!!
+                    alert(`api = ${api}`);   // ################## test, remove !!!
 
-            }  // if (typeof(data['chatgpt_api_key']...)
-        }
-    });  // $.get()
+                }  // if (typeof(data['chatgpt_api_key']...)
+            }
+        });  // $.get()
+    }
 });
