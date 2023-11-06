b31c138215b878ce225a2929fea4a11d9be3abd2
Chris Komaroff <ckom9@example.com>
2023-11-05 21:20:02 -0800

    ... work on chatgpt_api_js app ...

--- chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
+++ chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
@@ -1,18 +1,20 @@
 api = undefined;
 
 
-$("#prompt").on("click", function(event){
-    if (typeof(api)==="undefined") {
-        $.get("/chatgpt_api_js/test1redirect/", function (data) {
-            if (JSON.stringify(data) !== '{}') {
-                if (typeof (data['chatgpt_api_key']) !== "undefined" && data['chatgpt_api_key'] !== '') {
-                    // DANGER: api must be a local variable to prevent security hole!
-                    api = data['chatgpt_api_key']; // From Django
-
-                    alert(`api = ${api}`);   // ################## test, remove !!!
-
-                }  // if (typeof(data['chatgpt_api_key']...)
-            }
-        });  // $.get()
-    }
-});
+// ############### NOT GOOD SOLUTION 11/5/23 ###################################################
+// $("#prompt").on("click", function(event){
+//     if (typeof(api)==="undefined") {
+//         $.get("/chatgpt_api_js/test1redirect/", function (data) {
+//             if (JSON.stringify(data) !== '{}') {
+//                 if (typeof (data['chatgpt_api_key']) !== "undefined" && data['chatgpt_api_key'] !== '') {
+//                     // DANGER: api must be a local variable to prevent security hole!
+//                     api = data['chatgpt_api_key']; // From Django
+//
+//                     alert(`api = ${api}`);   // ################## test, remove !!!
+//
+//                 }  // if (typeof(data['chatgpt_api_key']...)
+//             }
+//         });  // $.get()
+//     }
+// });
+// ############### NOT GOOD SOLUTION 11/5/23 ###################################################
--- chatgpt_api_js/views.py
+++ chatgpt_api_js/views.py
@@ -7,30 +7,36 @@ from django.views import generic
 from . import forms
 
 
-class ChatGPT_API_JS_Test1(generic.TemplateView):
-    """ Access ChaptGPT API using (mostly) javascript.
-    	DANGER: Using Django like this make not be secure given API will be
-    	passed (via ajax) to javascript, BUT JAVASCRIPT GLOBAL VAR WILL NOT BE USED! 11/2/23
-    """
-    form_class = forms.ChatGPT_API_JS_Test1_Form
-    ## model = models.Taghouse
-    template_name = "chatgpt_api_js/chatgpt_api_js.html"
+class ChatGPT_API_JS_Test1(generic.UpdateView):
+    # Ref. Tagbirds, tagchapters/views.py, UpdateTagChapterEventFormFieldNameDescAjax
+    pass
 
-
-class ChatGPT_API_JS_Test1_Redirect(generic.RedirectView):
-    """ Send OpenAI ChatGPT API key to Javascript (security, hide it). 11/5/23
-    """
-    # # form_class = forms.ChatGPT_API_JS_Test1_Form  ############## Form not needed?
-    # ## model = models.
-    # template_name = "chatgpt_api_js/chatgpt_api_js.html"
-
-    def get_redirect_url(self, *args, **kwargs):
-        return reverse_lazy("chatgpt_api_js:chatgpt_api_js_test1")
-
-    @staticmethod
-    def get(request, *args, **kwargs):
-        """ ChatGPT_API_JS_Test1, get(), receives GET request from chatgpt_api_js.js $.get():
-            The OpenAI ChatGPT API key is returned (to chatgpt_api_js.js). This is security measure to hide the value
-            of API key. IMPORTANT: THE API KEY MUST NOT BE SAVED TO A JAVASCRIPT GLOBAL VARIABLE !!!.
-        """
-        return JsonResponse({'chatgpt_api_key': settings.OPENAI_CHATGPT_API_KEY})
+############### NOT GOOD SOLUTION 11/5/23 ###################################################
+# class ChatGPT_API_JS_Test1(generic.TemplateView):
+#     """ Access ChaptGPT API using (mostly) javascript.
+#     	DANGER: Using Django like this make not be secure given API will be
+#     	passed (via ajax) to javascript, BUT JAVASCRIPT GLOBAL VAR WILL NOT BE USED! 11/2/23
+#     """
+#     form_class = forms.ChatGPT_API_JS_Test1_Form
+#     ## model = models.Taghouse
+#     template_name = "chatgpt_api_js/chatgpt_api_js.html"
+#
+#
+# class ChatGPT_API_JS_Test1_Redirect(generic.RedirectView):
+#     """ Send OpenAI ChatGPT API key to Javascript (security, hide it). 11/5/23
+#     """
+#     # # form_class = forms.ChatGPT_API_JS_Test1_Form  ############## Form not needed?
+#     # ## model = models.
+#     # template_name = "chatgpt_api_js/chatgpt_api_js.html"
+#
+#     def get_redirect_url(self, *args, **kwargs):
+#         return reverse_lazy("chatgpt_api_js:chatgpt_api_js_test1")
+#
+#     @staticmethod
+#     def get(request, *args, **kwargs):
+#         """ ChatGPT_API_JS_Test1, get(), receives GET request from chatgpt_api_js.js $.get():
+#             The OpenAI ChatGPT API key is returned (to chatgpt_api_js.js). This is security measure to hide the value
+#             of API key. IMPORTANT: THE API KEY MUST NOT BE SAVED TO A JAVASCRIPT GLOBAL VARIABLE !!!.
+#         """
+#         return JsonResponse({'chatgpt_api_key': settings.OPENAI_CHATGPT_API_KEY})
+############### NOT GOOD SOLUTION 11/5/23 ###################################################
