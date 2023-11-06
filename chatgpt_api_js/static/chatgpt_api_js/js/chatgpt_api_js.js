eb447fab5fe3222d312496e8a7245e31dc525ec8
Chris Komaroff <ckom9@example.com>
2023-11-05 23:21:32 -0800

    ... work on chatgpt_api_js app ...

--- chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
+++ chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
@@ -1,19 +1,17 @@
 
 
-
 $("#id_prompt").on("click", function(event){
-
-    alert("Clicked #id_prompt");  // ######### tewst
-
     $.ajax({
         // type: "POST",  ######### old?
+        /* I don't believe I have ever used type "GET" for the jQuery ajax(). BUT I need to avoid error 403 problems,
+           and I only need to "get" the OpenAI ChatGPT API key and assign to a LOCAL(!) javascipt variable. So using
+           type "POST" is (luckily) unnecessary. 11/5/23 */
         type: "GET",  // ############# test?
         url: '/chatgpt_api_js/test1/',
         dataType: "json",
         data: {},
     })  // ajax()
     .done(function(data){
-        alert("done");  // ############## test
         if (typeof (data['chatgpt_api_key']) !== "undefined"){
             let api = data['chatgpt_api_key'];
             alert(`api = ${api}`);
--- chatgpt_api_js/views.py
+++ chatgpt_api_js/views.py
@@ -23,26 +23,6 @@ class ChatGPT_API_JS_Test1(generic.FormView):
             return JsonResponse({'chatgpt_api_key': settings.OPENAI_CHATGPT_API_KEY})
         return response
 
-    # def form_invalid(self, form):
-    #     print("... form_invalid() ...")  ############# test
-    #     response = super().form_invalid(form)
-    #     if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
-    #         return JsonResponse(form.errors, status=304)
-    #     return response
-
-    # def form_valid(self, form):
-    #     """Django docs say to call super().form_valid() before you call
-    #     is_ajax() 6/2/19
-    #     Django 3.1 request.is_ajax() is deprecated, see comments above 9/21/20
-    #     """
-    #
-    #     print(f"form.cleaned_data: {form.cleaned_data}")  ########## test
-    #
-    #     response = super().form_valid(form)
-    #     if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
-    #         return JsonResponse({'chatgpt_api_key': settings.OPENAI_CHATGPT_API_KEY})
-    #     return response
-
 
 class ChatGPT_API_JS_Test1_Prompt(generic.FormView):
     form_class = forms.ChatGPT_API_JS_Test1_Prompt_Form
@@ -52,33 +32,3 @@ class ChatGPT_API_JS_Test1_Prompt(generic.FormView):
     def get_success_url(self, *args, **kwargs):
         """ Does not leave the page """
         return reverse_lazy("chatgpt_api_js:chatgpt_api_js_test1")
-
-############### NOT GOOD SOLUTION 11/5/23 ###################################################
-# class ChatGPT_API_JS_Test1(generic.TemplateView):
-#     """ Access ChaptGPT API using (mostly) javascript.
-#     	DANGER: Using Django like this make not be secure given API will be
-#     	passed (via ajax) to javascript, BUT JAVASCRIPT GLOBAL VAR WILL NOT BE USED! 11/2/23
-#     """
-#     form_class = forms.ChatGPT_API_JS_Test1_Form
-#     ## model = models.Taghouse
-#     template_name = "chatgpt_api_js/chatgpt_api_js.html"
-#
-#
-# class ChatGPT_API_JS_Test1_Redirect(generic.RedirectView):
-#     """ Send OpenAI ChatGPT API key to Javascript (security, hide it). 11/5/23
-#     """
-#     # # form_class = forms.ChatGPT_API_JS_Test1_Form  ############## Form not needed?
-#     # ## model = models.
-#     # template_name = "chatgpt_api_js/chatgpt_api_js.html"
-#
-#     def get_redirect_url(self, *args, **kwargs):
-#         return reverse_lazy("chatgpt_api_js:chatgpt_api_js_test1")
-#
-#     @staticmethod
-#     def get(request, *args, **kwargs):
-#         """ ChatGPT_API_JS_Test1, get(), receives GET request from chatgpt_api_js.js $.get():
-#             The OpenAI ChatGPT API key is returned (to chatgpt_api_js.js). This is security measure to hide the value
-#             of API key. IMPORTANT: THE API KEY MUST NOT BE SAVED TO A JAVASCRIPT GLOBAL VARIABLE !!!.
-#         """
-#         return JsonResponse({'chatgpt_api_key': settings.OPENAI_CHATGPT_API_KEY})
-############### NOT GOOD SOLUTION 11/5/23 ###################################################
