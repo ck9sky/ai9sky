28a4ea73359dacf44456d9ecb6e17225401cb6c7
Chris Komaroff <ckom9@example.com>
2023-11-05 23:25:34 -0800

    ... work on chatgpt_api_js app ...

--- chatgpt_api_js/views.py
+++ chatgpt_api_js/views.py
@@ -8,16 +8,15 @@ from . import forms
 
 
 class ChatGPT_API_JS_Test1(generic.FormView):
-    # Ref. Tagbirds, tagchapters/views.py, UpdateTagChapterEventFormFieldNameDescAjax
     form_class = forms.ChatGPT_API_JS_Test1_Hidden_Form
-    ## model = models.xxxxxx
     template_name = "chatgpt_api_js/chatgpt_api_js.html"
 
     def get_success_url(self, *args, **kwargs):
         """ Does not leave the page """
         return reverse_lazy("chatgpt_api_js:chatgpt_api_js_test1")
 
-    def get(self, request, *args, **kwargs):  ############### ????????
+    def get(self, request, *args, **kwargs):
+        """ The form for this view never posts anything, and is just used to deliver api key to javascript. 11/5/23 """
         response = super().get(request, *args, **kwargs)
         if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
             return JsonResponse({'chatgpt_api_key': settings.OPENAI_CHATGPT_API_KEY})
