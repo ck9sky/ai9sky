78eb575a0ee98635d6ba6f9554b7bdec1fe69f85
Chris Komaroff <ckom9@example.com>
2023-11-05 20:11:47 -0800

    ... work on chatgpt_api_js app ...

--- chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
+++ chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
@@ -1,14 +1,14 @@
 
-$("#id_btn").on("click", function(event){
+$("#prompt").on("click", function(event){
     $.get("/user-accounts/refresh-session-key-browser-ajax/", function (data){
         if (JSON.stringify(data)!=='{}'){
             if (typeof(data['chatgpt_api_key'])!=="undefined" && data['chatgpt_api_key']!==''){
                 // DANGER: api must be a local variable to prevent security hole!
-                let api = data['session_key_now']; // From Django
+                let api = data['chatgpt_api_key']; // From Django
 
+                alert(`api = ${api}`);   // ################## test, remove !!!
 
-
-            }  // if (typeof(data['session_key_now']...)
+            }  // if (typeof(data['chatgpt_api_key']...)
         }
     });  // $.get()
 });
