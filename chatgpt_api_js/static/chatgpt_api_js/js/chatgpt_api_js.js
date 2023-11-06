7638a94b0501d743fd241580b8826d10e0eb1370
Chris Komaroff <ckom9@example.com>
2023-11-05 20:54:38 -0800

    ... work on chatgpt_api_js app ...

--- chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
+++ chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
@@ -1,10 +1,12 @@
+api = undefined;
+
 
 $("#prompt").on("click", function(event){
     $.get("/chatgpt_api_js/test1redirect/", function (data){
         if (JSON.stringify(data)!=='{}'){
             if (typeof(data['chatgpt_api_key'])!=="undefined" && data['chatgpt_api_key']!==''){
                 // DANGER: api must be a local variable to prevent security hole!
-                let api = data['chatgpt_api_key']; // From Django
+                api = data['chatgpt_api_key']; // From Django
 
                 alert(`api = ${api}`);   // ################## test, remove !!!
 
