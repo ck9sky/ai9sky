2fd729a95d8819784931b247da2b86f854f6460f
Chris Komaroff <ckom9@example.com>
2023-11-05 20:09:09 -0800

    ... configure new PyCharm project ...

--- chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
+++ chatgpt_api_js/static/chatgpt_api_js/js/chatgpt_api_js.js
@@ -1,2 +1,14 @@
 
-//api =
+$("#id_btn").on("click", function(event){
+    $.get("/user-accounts/refresh-session-key-browser-ajax/", function (data){
+        if (JSON.stringify(data)!=='{}'){
+            if (typeof(data['chatgpt_api_key'])!=="undefined" && data['chatgpt_api_key']!==''){
+                // DANGER: api must be a local variable to prevent security hole!
+                let api = data['session_key_now']; // From Django
+
+
+
+            }  // if (typeof(data['session_key_now']...)
+        }
+    });  // $.get()
+});
