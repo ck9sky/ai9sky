1747d1a271880676973c43b30e77403e7422016e
Chris Komaroff <ckom9@example.com>
2023-11-11 21:43:27 -0800

    ... debug new app 'chatgpt_api_py' ...

--- chatgpt_api_py/views.py
+++ chatgpt_api_py/views.py
@@ -22,47 +22,68 @@ class ChatGPT_API_PY_Test1(generic.FormView):
         """ Does not leave the page """
         return reverse_lazy("chatgpt_api_py:chatgpt_api_py_test1")
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        if self.plus_context:
-            if 'prompt' in self.plus_context:
-                context['prompt'] = self.plus_context['prompt']
-            else:
-                context['prompt'] = ""
-            if 'message' in self.plus_context:
-                context['message'] = self.plus_context['message']
-            else:
-                context['message'] = "Thinking"
-        else:
-            context['prompt'] = ""
-            context['message'] = "Thinking"
-
-        print(f"\ncontext = {context}\n")  ############# test
-
-        return context
+    ############### debug/remove???
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #
+    #     if self.plus_context:
+    #         if 'prompt' in self.plus_context:
+    #             context['prompt'] = self.plus_context['prompt']
+    #         else:
+    #             context['prompt'] = ""
+    #         if 'message' in self.plus_context:
+    #             context['message'] = self.plus_context['message']
+    #         else:
+    #             context['message'] = "Thinking"
+    #     else:
+    #         context['prompt'] = ""
+    #         context['message'] = "Thinking"
+    #
+    #     print(f"\n(1) context = {context}\nkwargs = {kwargs}")  ############# test
+    #
+    #     return context
 
     def form_valid(self, form):  ############# experiment
         # We will add code to show this message (or remove this message if not necessary. 11/11/23
-        question = form.cleaned_data['prompt']
-        if settings.NULL_STR.__eq__(question):
-            messages.error(
-                self.request,
-                "You Must Enter a Question!")
-            form.add_error('prompt', True)
-            return self.form_invalid(form)
-        """
-            Now we use Python library module openai. 11/11/23
-        """
-        client = OpenAI()
-        chat_completion = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": question}]
-        )
 
-        self.plus_context['prompt'] = question
-        self.plus_context['message'] = chat_completion.choices[0].message.content
+        print("\nform_valid() ...\n")  ############### test
         return super().form_valid(form)
+
+
+    # def form_valid(self, form):  ############# experiment
+    #     # We will add code to show this message (or remove this message if not necessary. 11/11/23
+    #
+    #     print("\nform_valid() ...\n")  ############### test
+    #
+    #     # question = form.cleaned_data['prompt']
+    #     # if settings.NULL_STR.__eq__(question):
+    #     #     messages.error(
+    #     #         self.request,
+    #     #         "You Must Enter a Question!")
+    #     #     form.add_error('prompt', True)
+    #     #     return self.form_invalid(form)
+    #     # """
+    #     #     Now we use Python library module openai. 11/11/23
+    #     # """
+    #     # client = OpenAI()
+    #     # chat_completion = client.chat.completions.create(
+    #     #     model="gpt-3.5-turbo",
+    #     #     messages=[{"role": "user", "content": question}]
+    #     # )
+    #     #
+    #     # kwargs = self.get_form_kwargs()
+    #     # context = self.get_context_data(**kwargs)
+    #     # print(f"\n(2) context = {context}\nkwargs = {kwargs}")  ############# test
+    #     #
+    #     # self.plus_context['prompt'] = question
+    #     # self.plus_context['message'] = chat_completion.choices[0].message.content
+    #
+    #     return super().form_valid(form)
+
+    # ################ causes error ??!!!
+    # def post(self, request, *args, **kwargs):
+    #     print("\npost() ...\n")  ############### test
+    #     super().post(request, *args, **kwargs)
 
 
 
