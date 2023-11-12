89ed0714fb084ddfa96ecb5afefa4f5e5102a942
Chris Komaroff <ckom9@example.com>
2023-11-11 17:42:51 -0800

    ... work on chatgpt_api_js app ...

--- chatgpt_api_py/static/chatgpt_api_py/js/chatgpt_api_py.js
+++ chatgpt_api_py/static/chatgpt_api_py/js/chatgpt_api_py.js
@@ -1,1 +1,13 @@
-// javascript
+// noinspection ES6ConvertVarToLetConst,LocalVariableNamingConventionJS
+//
+/* NOTE: Unlike app 'chaptgpt_api_js', this app uses Python openai library. Thus we probably can avoid the
+   complications of securely "sending" the api key to Javascript (local variable, etc.)  11/11/23
+ */
+
+
+$(function(){
+    /* NOTE: $(function()) is the jQuery ready function, equivalent to addEventListener("DOMContentLoaded").
+       Trick #1: unbind click event from id_prompt! User can click id_prompt box and nothing happens yet.
+     */
+
+});
