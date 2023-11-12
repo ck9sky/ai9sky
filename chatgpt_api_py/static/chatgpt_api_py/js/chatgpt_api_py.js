4a0f1cc17dfd69d74fe0be0051a9b51c68e861b0
Chris Komaroff <ckom9@example.com>
2023-11-11 22:29:51 -0800

    ... debug new app 'chatgpt_api_py' ...

--- chatgpt_api_py/static/chatgpt_api_py/js/chatgpt_api_py.js
+++ chatgpt_api_py/static/chatgpt_api_py/js/chatgpt_api_py.js
@@ -3,10 +3,49 @@
 /* NOTE: Unlike app 'chaptgpt_api_js', this app uses Python openai library. Thus we probably can avoid the
    complications of securely "sending" the api key to Javascript (local variable, etc.)  11/11/23
  */
+const prompt_form = document.querySelector('#prompt-form');
+const prompt_input = document.querySelector('#id_prompt');
+const chatLog = document.querySelector('.chat-log');
+
 
 $(function(){
     /* NOTE: $(function()) is the jQuery ready function, equivalent to addEventListener("DOMContentLoaded").
-       Trick #1: unbind click event from id_prompt! User can click id_prompt box and nothing happens yet.
      */
-
+    createMessageInstance();
 });
+
+
+function createMessageInstance(){
+    // Replace inner html of our chatlog container
+    // In video, he renames local var from 'value' to 'prompt'...
+    // noinspection HtmlUnknownTarget
+    chatLog.innerHTML +=
+    `
+    <div class="message-instance-container">
+        <div class="message user-message">
+            <div class="content">
+                <div class="message-image"></div>
+                <p>{{ prompt }}</p>
+            </div>
+        </div>
+        <div class="message ai-message">
+            <div class="content">
+                <div class="message-image">
+                    <img src="${iconStr}" alt="">
+                </div>
+                <p class="thinking">{{ message }}</p>
+            </div>
+        </div>
+    </div>    
+    `
+}
+
+
+prompt_form.addEventListener('submit', e => {
+
+    prompt_value = prompt_input.value;
+    if (prompt_value !== "") {
+        createMessageInstance();
+    }
+
+})
