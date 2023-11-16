20c4a04fe1b1690ff3e9c3f9fd13241f340c3b9c
Chris Komaroff <ckom9@example.com>
2023-11-16 00:20:42 -0800

    ... new app 'dalle_api_js' ...

---
+++ dalle_api_js/static/dalle_api_js/js/dalle_api_js.js
@@ -0,0 +1,12 @@
+// noinspection ES6ConvertVarToLetConst,LocalVariableNamingConventionJS
+//
+/* DANGER: api_key MUST BE a LOCAL VAR, NOT A GLOBAL VAR !!! A global var is easy to see in a browser!
+   This is a very IMPORTANT security feature.  11/15/23
+ */
+
+
+$(function(){
+    /* NOTE: $(function()) is the jQuery ready function, equivalent to addEventListener("DOMContentLoaded").
+       Trick #1: unbind click event from id_prompt! User can click id_prompt box and nothing happens yet.
+     */
+});
